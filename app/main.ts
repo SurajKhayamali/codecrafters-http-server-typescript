76389b0d3f1f581e0b65ca4ced2553e530816f2a
Suraj Khayamali <surajkhayamali.sk@gmail.com>
2024-05-25 09:48:10 +0545

    task: Respond with body

--- app/main.ts
+++ app/main.ts
@@ -28,15 +28,25 @@ const formHTTPResponse = (
   return response;
 };
 
+// Regex to match HTTP request in pattern GET /echo/{str} HTTP/1.1
+const echoPattern = /^GET \/echo\/(.+) HTTP\/1.1/;
+
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const requestString = data.toString();
     const headers = extractHeaders(requestString);
     console.log("Request headers:", headers);
     let response;
-    // 200 OK on slash, 404 on anything else
+    // 200 OK on slash and echo the string on /echo/{str}, 404 on anything else
     if (requestString.startsWith("GET / HTTP/1.1")) {
       response = formHTTPResponse(200, "OK");
+    } else if (echoPattern.test(requestString)) {
+      const echoMatch = echoPattern.exec(requestString) as RegExpExecArray;
+      response = formHTTPResponse(200, "OK", {
+        "Content-Type": "text/plain",
+        "Content-Length": echoMatch[1].length.toString(),
+      });
+      response += echoMatch[1];
     } else {
       response = formHTTPResponse(404, "Not Found");
     }
